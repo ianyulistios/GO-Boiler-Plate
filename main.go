975c447a6608236e059bc8e0c2fc324db7472c13
ian yulistio <ian_yulistio_gue@ian.yulistio.gue>
2020-01-07 10:20:10 +0700

    Add new Command For Migration And For Serve

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"GO-Boiler-Plate/config"
+	"GO-Boiler-Plate/helpers"
+	"GO-Boiler-Plate/migration/tables"
 	"GO-Boiler-Plate/routes"
+	"fmt"
 
 	"github.com/labstack/echo"
 )
@@ -10,6 +13,15 @@ import (
 func main() {
 	config.Env()
 	e := echo.New()
+	switch helpers.Command() {
+	case "migrate":
+		tables.ExecuteMigration()
+		break
+	default:
+		fmt.Println("Invalid command")
+	}
 	routes.API(e)
-	e.Logger.Fatal(e.Start(":1412"))
+	if helpers.Command() != "migrate" {
+		e.Logger.Fatal(e.Start(":1412"))
+	}
 }
