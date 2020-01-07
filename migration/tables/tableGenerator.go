dfb8be37186897f86236c1bc5fcf0ff706b3bf6b
ian yulistio <ian_yulistio_gue@ian.yulistio.gue>
2020-01-07 10:25:49 +0700

    add new print for migration process

--- migration/tables/tableGenerator.go
+++ migration/tables/tableGenerator.go
@@ -7,14 +7,16 @@ import (
 
 // ExecuteMigration of Table
 func ExecuteMigration() {
-	fmt.Print("------EXECUTE THE MIGRATION---------")
+	fmt.Print("------EXECUTE THE MIGRATION---------\n")
 	TableSchema()
-	fmt.Print("-----------THE MIGRATION IS SUCCESS-----------")
+	fmt.Print("-----------THE MIGRATION IS SUCCESS-----------\n")
 }
 
 // TableSchema Represented Schema To Migrate The Model
 func TableSchema() {
+	fmt.Print("Process To Migrate\n")
 	databases.GetConnection().AutoMigrate(
 		User{},
 	)
+	fmt.Print("End of Process\n")
 }
